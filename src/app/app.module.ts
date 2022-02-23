126aea9af83531777c10f342fd2ccd183e9ad927
new user <s2101121@edu.bc.fi>
2022-02-23 12:58:44 +0200

    Tilanne osa 4. Add Services https://angular.io/tutorial/toh-pt4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
  
 
 @NgModule({
@@ -13,6 +14,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
+    MessagesComponent
     
   ],
   imports: [
